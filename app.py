141d2966e9f3aa2fac8801d37925ce078d8c780c
Serhii-Dub <im-24-dubrovin@kdpu.edu.ua>
2024-12-27 14:41:41 +0200

    Добавлены новые файлы

--- app.py
+++ app.py
@@ -1,30 +1,11 @@
-import tensorflow as tf
-import numpy as np
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
+from flask import Flask, render_template, request
+import os
 
-# Завантажуємо попередньо натреновану модель ResNet50
-model = ResNet50(weights='imagenet')
+app = Flask(__name__)
 
-# Завантажуємо зображення
-img_path = 'path_to_image.jpg'  # Вкажіть шлях до зображення
-img = image.load_img(img_path, target_size=(224, 224))
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-# Перетворюємо зображення в тензор
-img_array = image.img_to_array(img)
-
-# Додаємо розмірність, необхідну для моделі
-img_array = np.expand_dims(img_array, axis=0)
-
-# Попередня обробка зображення для ResNet50
-img_array = preprocess_input(img_array)
-
-# Передбачення
-predictions = model.predict(img_array)
-
-# Розшифровка результатів (отримуємо назви класів)
-decoded_predictions = decode_predictions(predictions, top=3)[0]
-
-# Виводимо топ-3 передбачення
-for i, (imagenet_id, label, score) in enumerate(decoded_predictions):
-    print(f"{i+1}: {label} ({score:.2f})")
+if __name__ == '__main__':
+    app.run(debug=True)
